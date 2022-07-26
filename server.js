d5512cd6b6d0601d02a5e724368b2c9a96b53b57
Brice Veyna <bricev99@gmail.com>
2022-07-25 20:36:38 -0700

    Add read/write fuctionality in post for note

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
@@ -17,6 +18,50 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.post('/api/notes', (req, res) => {
+
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(newNote);
+                notes = parsedNotes;
+
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully updated notes!')
+                );
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.json(response);
+    } else {
+        res.json('Error in adding note')
+    }
+});
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
